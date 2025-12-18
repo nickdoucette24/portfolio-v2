271538c6b887697beec9a1b26d87a049ab310285
Nick Doucette <nickdoucette24@gmail.com>
2025-12-17 23:05:59 -0500

    added dark mode navbar badge variant

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -10,14 +10,14 @@ export function Navbar() {
   return (
     <nav className="p-4 flex items-center justify-between h-16 border-b border-border">
       <SidebarTrigger className="hover:cursor-pointer" />
-      <div className="flex gap-4">
+      <div className="flex gap-4 items-center">
         <Badge
           variant="outline"
-          className="border-2 p-0.5 px-2 text-navbar-badge hover:cursor-default rounded-full"
+          className="border-2 h-7.5 px-2 border-navbar-badge text-background dark:text-navbar-badge bg-navbar-badge shadow-0 dark:bg-sidebar hover:cursor-default rounded-full"
         >
           <CircleSmall
             fill="lightgreen"
-            className="mr-0.5 h-6 w-6 animate-pulse"
+            className="mr-0.5 h-5 w-6 animate-pulse"
           />
           <span className="mr-1">Open to Work</span>
         </Badge>
